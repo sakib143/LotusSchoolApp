4f19b4541ecb4d60bdff1e7053cfaf066f8c1cbf
Sakib <syed.sakib143@gmail.com>
2020-10-19 21:24:31 -0700

    Uploading assignment DONE......Sakib Syed

--- app/src/main/java/com/appforschool/ui/home/HomeActivity.kt
+++ app/src/main/java/com/appforschool/ui/home/HomeActivity.kt
@@ -241,6 +241,7 @@ class HomeActivity : BaseBindingActivity<ActivityHomeBinding>(),
 
     private val fileSubmitObserver = Observer<AssignmentSubmissionModel> {
         if (it.status) {
+            openAssignmentFragment()
             toast(it.message)
         } else {
             toast(it.message)
