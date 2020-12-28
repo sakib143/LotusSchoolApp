404de5ffd7b196d290cb68fe84dd0670d8d4a829
sakib143 <syed.sakib143@gmail.com>
2020-12-28 12:36:02 +0530

    Request timeout added in Network calling....Sakib Syed

--- app/src/main/java/com/appforschool/di/NetworkModule.kt
+++ app/src/main/java/com/appforschool/di/NetworkModule.kt
@@ -17,6 +17,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
@@ -36,7 +37,11 @@ class NetworkModule {
      * */
     @Singleton
     @Provides
-    fun providesOkHttp(prefUtils: PrefUtils): OkHttpClient = OkHttpClient.Builder()
+    fun providesOkHttp(prefUtils: PrefUtils): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
