8f2abc78816fd5dfb291b0e3e8735b27a54cdddb
sakib143 <syed.sakib143@gmail.com>
2021-02-05 13:20:59 +0530

    Image crop in Profile pic lib changed...Sakib Syed

--- app/src/main/java/com/appforschool/ui/profile/user/UserProfileActivity.kt
+++ app/src/main/java/com/appforschool/ui/profile/user/UserProfileActivity.kt
@@ -2,20 +2,22 @@ package com.appforschool.ui.profile.user
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.appforschool.R
 import com.appforschool.base.BaseBindingActivity
 import com.appforschool.data.model.ChangeProfilePicModel
-import com.appforschool.data.model.LoginModel
 import com.appforschool.data.model.UpdateProfileModel
 import com.appforschool.databinding.ActivityUserProfileBinding
 import com.appforschool.listner.UserProfileListner
 import com.appforschool.utils.LogM
 import com.appforschool.utils.toast
-import com.theartofdev.edmodo.cropper.CropImage
-import com.theartofdev.edmodo.cropper.CropImageView
+import com.yalantis.ucrop.UCrop
 import java.io.File
 import javax.inject.Inject
 
@@ -72,24 +74,61 @@ class UserProfileActivity : BaseBindingActivity<ActivityUserProfileBinding>() {
     }
 
     fun openGallery() {
-        CropImage.activity()
-            .setGuidelines(CropImageView.Guidelines.ON)
-            .start(this@UserProfileActivity)
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+            .setType("image/*")
+            .addCategory(Intent.CATEGORY_OPENABLE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val mimeTypes = arrayOf("image/jpeg", "image/png")
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        }
+
+        startActivityForResult(Intent.createChooser(intent, "Select picture"), 1)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            val result = CropImage.getActivityResult(data)
-            if (resultCode == RESULT_OK) {
-                val imageUri = result.uri
-                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
-                viewModel.selectedBitmapImage.value = bitmap
-                viewModel.imagePath.value = File(result.uri.path)
-                viewModel.executeChangeProfilePic()
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                val error = result.error
+        if (requestCode == 1) {
+            val selectedUri = data!!.data
+            if (selectedUri != null) {
+                startCrop(selectedUri)
+            } else {
+                toast("Unable to retrieve image")
             }
+        } else if (requestCode == UCrop.REQUEST_CROP) {
+            handleCropResult(data!!)
         }
+
+        if (resultCode == UCrop.RESULT_ERROR) {
+            handleCropError(data!!)
+        }
+    }
+
+    private fun startCrop(uri: Uri) {
+        val options = UCrop.Options()
+        options.setFreeStyleCropEnabled(true)
+        options.setHideBottomControls(true)
+
+        val destinationFileName = ".jpg"
+        val uCrop = UCrop.of(uri, Uri.fromFile(File(cacheDir, destinationFileName)))
+        uCrop.withOptions(options)
+        uCrop.start(this@UserProfileActivity)
+    }
+
+    private fun handleCropError(result: Intent) {
+        val cropError = UCrop.getError(result)
+        if (cropError != null) {
+            toast("Something went wrong.Please try again")
+        } else {
+            toast("Something went wrong.Please try again")
+        }
+    }
+
+    private fun handleCropResult(result: Intent) {
+        val resultUri = UCrop.getOutput(result)
+        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, resultUri)
+        viewModel.selectedBitmapImage.value = bitmap
+        viewModel.imagePath.value = File(resultUri?.path)
+        viewModel.executeChangeProfilePic()
     }
 }
